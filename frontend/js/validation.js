95cb52afe7c6337514dbb4db7a380d8f7608f800
João Marcos de Andrade Silva <jmda363959@outlook.com>
2025-10-07 09:19:51 -0300

    Refatoração do código de validação do form login
    Algumas alterações: Funções para pegar os elementos modificados para variáveis,
    código simplificado, remoção de alguns comentários (Talvez haja mais alterações
    se desejar, é só ver a diferença)

--- frontend/js/validation.js
+++ frontend/js/validation.js
@@ -1,94 +1,86 @@
 const loginForm = document.querySelector('#loginForm'); // Pegando o formulário
 
+const getName = document.getElementById('name');
+const getEmail = document.getElementById('email');
+const getPassword = document.getElementById('password');
+const getConfirmPassword = document.getElementById('confirm-password');
+
 loginForm.addEventListener('submit', (event) => {
   event.preventDefault();
   formValidation();
 });
 
-// Função para validação do formulário
 function formValidation() {
-  const nameForm = getName(); // Pegar o nome do usuário
-  const emailForm = getEmail(); // Pegar o email do usuário 
-  const passwordForm = getPassword(); // Pegar a senha do usuário
-  const confirmPasswordForm = getConfirmPassword(); // Pegar a confirmação de senha do usuário
-  const minTextLengthName = 3; // Quantidade mínima de caracteres do nome
-  const minTextLengthPassword = 8; // Quantidade mínima de caracteres da senha
-  
-  // saber es todos os campo estão falidos ou não
-  let valid = true; 
+  const minTextLengthName = 3;
+  const minTextLengthPassword = 8;
 
-  // Validação do campo de nome
-  
-  // Variável com a função de validar o tamanho do input
-  const nameValidationResult = minLength(nameForm, minTextLengthName); // validando o tamanho do nome
+  // Saber es todos os campo estão validos ou não
+  let valid = true;
 
-  if (!nameValidationResult && !isInputEmpty(nameForm.value.trim())) {
-    errorMensage(nameForm, `Deve possuir no mínimo ${minTextLengthName} caracteres`);
-    
+  const nameLengthValidation = minLength(getName, minTextLengthName); // validando o tamanho do nome
+
+  if (isInputEmpty(getName.value.trim())) {
+    errorMensage(getName, 'Este campo é obrigatório.');
+
     valid = false;
-  } else if (isInputEmpty(nameForm.value.trim())) {
-    errorMensage(nameForm, 'Este campo é obrigatório.');
-    
+  } else if (nameLengthValidation) {
+    errorMensage(getName, `Deve possuir no mínimo ${minTextLengthName} caracteres`);
+
     valid = false;
   } else {
-
-    errorMensage(nameForm, '', true)
-
+    errorMensage(getName, '', true)
   }
 
   // Validação do campo de email
 
   // Variavel com o resultado da validação do email
-  const emailValidationResult = isValidEmail(emailForm.value.trim()); // Validando se o email está correto ou não
 
-  if (!emailValidationResult && !isInputEmpty(emailForm.value.trim())) {
-    errorMensage(emailForm, 'Digite um email valido. Ex.: example@example.com')
+  if (isInputEmpty(getEmail.value.trim())) {
+    errorMensage(getEmail, 'Este campo é obrigatório.');
 
     valid = false
-  } else if (isInputEmpty(emailForm.value.trim())) {
-    errorMensage(emailForm, 'Este campo é obrigatório.');
+  } else if (!isValidEmail(getEmail.value.trim())) {
+    errorMensage(getEmail, 'Digite um email valido. Ex.: example@example.com')
 
     valid = false;
   } else {
-
-    errorMensage(emailForm, '', true)
-
+    errorMensage(getEmail, '', true)
   }
 
   // Validação do campo de senha
-  
+
   // Variavel com o resultado da validação do tamanho da senha
-  const passwordValidationResult = minLength(passwordForm, minTextLengthPassword); // validando o tamanho da senha
-  
-  if (!passwordValidationResult && !isInputEmpty(passwordForm.value.trim())) {
-    errorMensage(passwordForm, `Deve possuir no mínimo ${minTextLengthPassword} caracteres`);
+  const passwordValidationResult = minLength(getPassword, minTextLengthPassword); // validando o tamanho da senha
+
+  if (!passwordValidationResult && !isInputEmpty(getPassword.value.trim())) {
+    errorMensage(getPassword, `Deve possuir no mínimo ${minTextLengthPassword} caracteres`);
 
     valid = false;
-  } else if (isInputEmpty(passwordForm.value.trim())) {
-    errorMensage(passwordForm, 'Este campo é obrigatório.');
+  } else if (isInputEmpty(getPassword.value.trim())) {
+    errorMensage(getPassword, 'Este campo é obrigatório.');
 
     valid = false;
   } else {
-    errorMensage(passwordForm, '', true)
+    errorMensage(getPassword, '', true)
 
   }
-  
+
   // Validação da senha de confirmação
 
   // Variavel com o resultado da validação do tamanho da senha de confirmação
   // validando o tamanho da senha de confirmação
-  const confirmPasswordValidationResult = minLength(confirmPasswordForm, minTextLengthPassword);
-  
-  if (!confirmPasswordValidationResult && !isInputEmpty(passwordForm.value.trim())) {
-    errorMensage(confirmPasswordForm, `Deve possuir no mínimo ${minTextLengthPassword} caracteres`);
-    
+  const confirmPasswordValidationResult = minLength(getConfirmPassword, minTextLengthPassword);
+
+  if (!confirmPasswordValidationResult && !isInputEmpty(getPassword.value.trim())) {
+    errorMensage(getConfirmPassword, `Deve possuir no mínimo ${minTextLengthPassword} caracteres`);
+
     valid = false;
-  } else if (isInputEmpty(confirmPasswordForm.value.trim())) {
-    errorMensage(confirmPasswordForm, 'Este campo é obrigatório.');
+  } else if (isInputEmpty(getConfirmPassword.value.trim())) {
+    errorMensage(getConfirmPassword, 'Este campo é obrigatório.');
 
     valid = false;
   } else {
-    errorMensage(confirmPasswordForm, '', true)
+    errorMensage(getConfirmPassword, '', true)
 
   }
 
@@ -96,43 +88,23 @@ function formValidation() {
 
   // Variavel com o resutado da validação das senhas
   // verificar se as senhas estão correta 
-  const validationPasswordsResult = validationPasswords(passwordForm, confirmPasswordForm);
-  
+  const validationPasswordsResult = validationPasswords(getPassword, getConfirmPassword);
+
   if (!validationPasswordsResult && passwordValidationResult && confirmPasswordValidationResult) {
-    errorMensage(passwordForm, 'Erro! Senhas diferentes.')
-    errorMensage(confirmPasswordForm, 'Erro! Senhas diferentes.');
+    errorMensage(getPassword, 'Erro! Senhas diferentes.')
+    errorMensage(getConfirmPassword, 'Erro! Senhas diferentes.');
     valid = false;
   }
 
   // Verificação se já está valido para mudar de página
-  if(valid) {
+  if (valid) {
     window.location.href = "./html/main-page.html"
   }
 }
 
-// Pegar o nome
-function getName() {
-  return document.querySelector('#name');
-}
-
-// Pegar o Email
-function getEmail() {
-  return document.querySelector('#email');
-}
-
-// Pegar a senha
-function getPassword() {
-  return document.querySelector('#password');
-}
-
-// Pegar a senha de confirmar
-function getConfirmPassword() {
-  return document.querySelector('#confirm-password');
-}
-
 // Retorna se o valor está vazio(true) ou não(false)
-function isInputEmpty(input){
-  return input === '';
+function isInputEmpty(input) {
+  return !input;
 }
 
 // colocar e retirar a mensagem de erro
@@ -142,12 +114,12 @@ function errorMensage(input, text = '', clean = false) {
   input: serve para escolher qual input com uma tag seguinte (sendo o span) vai ser aplicado este aviso;
   text: texto da mensagem;
   clean: limpa o texto da mensagem se for true, se for false, não faz nada.
-  */ 
- 
+  */
+
   // Limpar a mensagem de erro 
   if (clean === true) {
     input.nextElementSibling.innerText = "";
-    return input.classList.remove('required');  
+    return input.classList.remove('required');
   }
 
   input.classList.add('required');
@@ -163,30 +135,21 @@ function minLength(input, length) {
   input: escolher qual é o input que vai ser verificado;
   length: tamanho mínimo de caracteres no input.
   */
-  
-  if(input.value.trim().length < length && input.value.trim() != '') {
-    input.classList.add('required');
 
-    return false;
-  } else if(input.value.trim().length < length) {
+  if (input.value.trim().length < length) {
     input.classList.add('required');
-
     return false;
-  } 
-  
-  else  {
-    
-    input.classList.remove('required');
-    
-    return true;
   }
+
+  input.classList.remove('required');
+  return true;
 }
 
 // Retorna se o email é valido(true) ou não(false)
 function isValidEmail(email) {
   const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@(([^<>()[\]\\.,;:\s@"]+\.)+[^<>()[\]\\.,;:\s@"]{2,})$/i;
 
-  return emailRegex.test(email); 
+  return emailRegex.test(email);
 }
 
 // Validadar as senhas estão iguais, sim(true) ou não(false)
