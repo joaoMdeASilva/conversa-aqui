22835ada19063680ee2b6ff910c010905b971f6d
João Marcos de Andrade Silva <jmda363959@outlook.com>
2025-10-20 09:02:49 -0300

    validação na class UserPass. construct. Ver descr.
    Além disso, os nomes dos seus atributos foram modificados, adicionando
    um "Element" ao final, para ficar mais claro que o argumento recebe um
    elemento HTML (Ex.: "#minPasswordElement").

--- frontend/js/classes/validation/UserPasswordValidation.js
+++ frontend/js/classes/validation/UserPasswordValidation.js
@@ -1,56 +1,67 @@
 import Helper from './Helper.js'
 
 export default class UserPasswordValidation {
-    #password;
-    #confirmPassword
+    #passwordElement;
+    #confirmPasswordElement;
     #minPassLength;
 
-    constructor(minPassLength, password, confirmPassoword = undefined) {
-        this.#password = password;
-        this.#confirmPassword = confirmPassoword;
+    constructor(minPassLength, passwordElement, confirmPassowordElement = undefined) {
+        if (!Number.isInteger(minPassLength))
+            throw new TypeError(Helper.typeOrInstanceErrorMessageTemplate('minPassLength', 'integer Number'));
+        else if(minPassLength < 0)
+            throw new RangeError(`"minPassLength" argument must be greater than or equal to 0. Received ${minPassLength}.`);
+
+        if (!Helper.isHtmlElement(passwordElement))
+            throw new TypeError(Helper.typeOrInstanceErrorMessageTemplate('passwordElement', 'HTMLElement', 'instance'))
+        
+        if (!Helper.isHtmlElement(confirmPassowordElement) && confirmPassowordElement) 
+            throw new TypeError(Helper.typeOrInstanceErrorMessageTemplate('confirPasswordElement', 'HTMLElement', 'instance'))
+        
         this.#minPassLength = minPassLength;
+        this.#passwordElement = passwordElement;
+        this.#confirmPasswordElement = confirmPassowordElement;
     }
 
     isUserPasswordAndConfirmPasswordEqual() {
         this.#isUserConfirmPasswordMissing();
 
-        return this.#password.value === this.#confirmPassword.value;
+        return this.#passwordElement.value === this.#confirmPasswordElement.value;
     }
 
     isUserPasswordValid() {
-        if (Helper.isInputEmpty(this.#password.value.trim())) {
-            Helper.errorMessage(this.#password, 'Este campo é obrigatório.');
+        if (Helper.isInputEmpty(this.#passwordElement.value.trim())) {
+            Helper.errorMessage(this.#passwordElement, 'Este campo é obrigatório.');
 
             return false;
-        } else if (!Helper.minLength(this.#password, this.#minPassLength)) {
-            Helper.errorMessage(this.#password, `Deve possuir no mínimo ${this.#minPassLength} caracteres`);
+        } else if (!Helper.minLength(this.#passwordElement, this.#minPassLength)) {
+            Helper.errorMessage(this.#passwordElement, `Deve possuir no mínimo ${this.#minPassLength} caracteres`);
 
             return false;
         }
 
-        Helper.errorMessage(this.#password, '', true);
+        Helper.errorMessage(this.#passwordElement, '', true);
         return true;
     }
 
     isUserConfirmPasswordValid() {
         this.#isUserConfirmPasswordMissing();
 
-        if (Helper.isInputEmpty(this.#confirmPassword.value.trim())) {
-            Helper.errorMessage(this.#confirmPassword, 'Este campo é obrigatório.');
+        if (Helper.isInputEmpty(this.#confirmPasswordElement.value.trim())) {
+            Helper.errorMessage(this.#confirmPasswordElement, 'Este campo é obrigatório.');
 
             return false;
-        } else if (!Helper.minLength(this.#confirmPassword, this.#minPassLength)) {
-            Helper.errorMessage(this.#confirmPassword, `Deve possuir no mínimo ${this.#minPassLength} caracteres`);
+        } else if (!Helper.minLength(this.#confirmPasswordElement, this.#minPassLength)) {
+            Helper.errorMessage(this.#confirmPasswordElement, `Deve possuir no mínimo ${this.#minPassLength} caracteres`);
 
             return false;
         }
-        Helper.errorMessage(this.#confirmPassword, '', true)
-        
+        Helper.errorMessage(this.#confirmPasswordElement, '', true)
+
         return true;
     }
 
     #isUserConfirmPasswordMissing() {
-        if (!this.#confirmPassword)
+        if (!this.#confirmPasswordElement)
             throw "'confirmPassword' is missing! You must pass it to the constructor.";
     }
 }
